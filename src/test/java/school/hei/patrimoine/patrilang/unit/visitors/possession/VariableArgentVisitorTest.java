faa856fe61d68290efe53c46de268551c8d21f0e
Sheddy <hei.sheddy.2@gmail.com>
2025-08-14 16:16:14 +0300

    test: Argent context with complexe expression test

--- src/test/java/school/hei/patrimoine/patrilang/unit/visitors/possession/VariableArgentVisitorTest.java
+++ src/test/java/school/hei/patrimoine/patrilang/unit/visitors/possession/VariableArgentVisitorTest.java
@@ -28,7 +28,7 @@ class VariableArgentVisitorTest {
   UnitTestVisitor visitor =
       new UnitTestVisitor() {
         @Override
-        public Argent visitArgent(ArgentContext ctx) {
+        public Object visitArgent(ArgentContext ctx) {
           return subject.apply(ctx);
         }
       };
@@ -105,6 +105,51 @@ class VariableArgentVisitorTest {
     variableVisitor.addToScope("valeurOrdinateur", NOMBRE, (double) valeurOrdinateur);
     var actual = visitor.visit(input, PatriLangParser::argent);
 
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void parse_argent_with_addition() {
+    var input = "(300000Ar + 450000Ar) évalué le 01 Juin 2025";
+    var expected = ariary(750_000);
+
+    var actual = visitor.visit(input, PatriLangParser::argent);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void parse_argent_with_subtraction() {
+    var input = "(500000Ar - 200000Ar) évalué le 01 Mai 2025";
+    var expected = ariary(300_000);
+
+    var actual = visitor.visit(input, PatriLangParser::argent);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void parse_argent_with_multiplication_by_number() {
+    var input = "(300000Ar * 2)";
+    var expected = ariary(600_000);
+
+    var actual = visitor.visit(input, PatriLangParser::argent);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void parse_argent_with_division_by_number() {
+    var input = "(600000Ar / 2)";
+    var expected = ariary(300_000);
+
+    var actual = visitor.visit(input, PatriLangParser::argent);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void parse_argent_all_complexe_operation() {
+    var input = "((((50000Ar - (-50000Ar)) / 2) * 5) * 5) évalué le 01 Juin 2025";
+    var expected = ariary(1_250_000);
+
+    var actual = visitor.visit(input, PatriLangParser::argent);
     assertEquals(expected, actual);
   }
 }
